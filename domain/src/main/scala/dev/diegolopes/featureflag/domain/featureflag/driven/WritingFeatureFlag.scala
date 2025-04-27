c975f28bd93ec187bca7da3820cfbf96ecbd1aed
Diego Pereira Lopes <diegopereiralopes98@gmail.com>
2025-04-27 16:10:24 -0300

    feat(domain): add update and delete to WritingFeatureFlag driven port trait

--- domain/src/main/scala/dev/diegolopes/featureflag/domain/featureflag/driven/WritingFeatureFlag.scala
+++ domain/src/main/scala/dev/diegolopes/featureflag/domain/featureflag/driven/WritingFeatureFlag.scala
@@ -1,7 +1,9 @@
 package dev.diegolopes.featureflag.domain.featureflag.driven
 
-import dev.diegolopes.featureflag.domain.featureflag.FeatureFlag
+import dev.diegolopes.featureflag.domain.featureflag.{FeatureFlag, FeatureFlagId}
 
 trait WritingFeatureFlag[F[_]] {
   def save(featureFlag: FeatureFlag): F[Unit]
+  def update(featureFlag: FeatureFlag): F[Unit]
+  def delete(id: FeatureFlagId): F[Unit]
 }
