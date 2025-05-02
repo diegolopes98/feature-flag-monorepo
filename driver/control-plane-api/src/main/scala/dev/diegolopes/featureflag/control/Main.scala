2abbeaff92d93a097d2465b1389be3290508611f
Diego Pereira Lopes <diegopereiralopes98@gmail.com>
2025-05-02 10:57:01 -0300

    feat(driver): use all layers in control plane main

--- driver/control-plane-api/src/main/scala/dev/diegolopes/featureflag/control/Main.scala
+++ driver/control-plane-api/src/main/scala/dev/diegolopes/featureflag/control/Main.scala
@@ -1,13 +1,24 @@
 package dev.diegolopes.featureflag.control
 
 import dev.diegolopes.featureflag.control.config.ControlPlaneConfig
+import dev.diegolopes.featureflag.platform.kafka.KafkaProducer
+import dev.diegolopes.featureflag.platform.postgres.PostgresDatabase
+import io.github.gaelrenoux.tranzactio.doobie.Database
 import zio.*
+import zio.kafka.producer.Producer
 
 object Main extends ZIOAppDefault {
   private val effect = for {
-    config <- ZIO.service[ControlPlaneConfig]
-    _      <- Console.printLine(config)
+    _ <- ZIO.service[ControlPlaneConfig]
+    _ <- ZIO.service[Producer]
+    _ <- ZIO.service[Database]
   } yield ()
 
-  def run: Task[Unit] = effect.provideLayer(ControlPlaneConfig.layer)
+  def run: Task[Unit] = effect.provideLayer {
+    ControlPlaneConfig.layer ++
+      (ControlPlaneConfig.layer >>> (ZLayer
+        .fromFunction((cfg: ControlPlaneConfig) => cfg.database) >>> PostgresDatabase.layer)) ++
+      (ControlPlaneConfig.layer >>> (ZLayer
+        .fromFunction((cfg: ControlPlaneConfig) => cfg.kafka) >>> KafkaProducer.layer))
+  }
 }
